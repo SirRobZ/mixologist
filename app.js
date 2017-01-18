128d6a2c02eaccf5d51930b14c07cfda8f9092fb
SirRobZ <sazarapkar@gmail.com>
2017-01-18 11:01:23 +0800

    added functions to bind buttons.

--- app.js
+++ app.js
@@ -1,5 +1,138 @@
+
+var state = {
+	liquors: ['Gin','Vodka','Bourbon','Scotch','Tequila','Rum','Brandy/Cognac','White Wine','Red Wine','Beer'],
+	mixers: ['Club Soda','Tonic','Cola','Lemon/Lime Soda','Ginger Ale','Orange Juice','Cranberry Juice','Tomato Juice','Pineapple Juice','Agnostura Bitters'],
+	results: [],
+	selectedliquors: [],
+	selectedmixers: [],
+}
+
 function main() {
 
+	function renderLiquorList() {
+		var liquorListHTML = state.liquors.map(function(liquor){
+			return('<li class="ingredient">'+ liquor +'</li>')
+		})
+		$('.liquors ul').html(liquorListHTML);
+	}
+
+	function renderMixerList() {
+		var mixerListHTML = state.mixers.map(function(mixer){
+			return('<li class="ingredient">'+ mixer +'</li>')
+		})
+		$('.mixers ul').html(mixerListHTML);
+	}
+
+	function renderResultsList() {
+		var resultsListHTML = state.results.map(function(result){
+			return('<li class="results">'+ result +'</li>')
+		})
+		$('.results ul').html(resultListHTML);
+	}
+
+	function generateResults() {
+		/*
+			create new array of liquors and mixers by joining. 
+			make API call to recipe puppy
+			pass response to render results list.
+		*/
+	}
+
+	function bindLiquorClick() {
+		/* 
+			on click add 'highlight' class
+			push name or index into state.selectedliquors 
+			toggle class and remove from state.selected liquors if clicked again.
+		*/
+		$('.liquors li').on('click', function(event){
+			$(this).toggleClass('highlight');
+			if ($(this).hasClass('highlight')) {
+				state.selectedliquors.push($(this).text());
+			} else {
+				var index = state.selectedliquors.indexOf($(this).text())
+				state.selectedliquors.splice(index, 1);
+			}
+		})
+	}
+
+	function bindLiquorListNextButton() {
+		/* 
+			hide liquors list
+			render mixer list and show.
+		*/
+		$('.liquors button').on('click', function(event) {
+			renderMixerList();
+			$('.liquors').addClass('hidden');
+			$('.mixers').removeClass('hidden');
+		})
+	}
+
+	function bindMixerClick() {
+		/* 
+			on click add 'highlight' class
+			push name or index into state.selectedmixers
+			toggle class and remove from state.selected liquors if clicked again.
+		*/
+		$('.mixers ul').on('click', 'li', function(event) {
+			$(this).toggleClass('highlight');
+			if ($(this).hasClass('highlight')) {
+				state.selectedmixers.push($(this).text());
+			} else {
+				var index = state.selectedmixers.indexOf($(this).text())
+				state.selectedmixers.splice(index, 1);
+			}
+		})
+	}
+
+	function bindMixerListNextButton() {
+		/* 
+			hide mixer list
+			genereate results
+		*/
+		$('.mixers .next').on('click', function(event){
+			$('.mixers').addClass('hidden');
+			$('.results').removeClass('hidden');
+		})
+	}
+
+	function bindMixerListBackButton() {
+		/* 
+			hide mixer list
+			genereate results
+		*/
+		$('.mixers .back').on('click', function(event){
+			$('.mixers').addClass('hidden');
+			$('.liquors').removeClass('hidden');
+		})
+	}
+
+	function bindResultsBackButton() {
+		$('.results .back').on('click', function(event){
+			$('.results').addClass('hidden');
+			$('.mixers').removeClass('hidden');
+		})
+	}
+
+	function bindResultsResetButton() {
+		$('.results .reset').on('click', function(event){
+			state.selectedliquors = [];
+			state.selectedmixers = [];
+			$('liquors li').removeClass('highlight');
+			$('mixers li').removeClass('highlight');
+			$('.results').addClass('hidden');
+			$('.liquors').removeClass('hidden');
+		})
+	}
+
+
+	renderLiquorList();
+	bindLiquorClick();
+	bindLiquorListNextButton();
+	bindMixerClick();
+	bindMixerListBackButton();
+	bindMixerListNextButton();
+	bindResultsBackButton();
+	bindResultsResetButton();
 }
 
 
